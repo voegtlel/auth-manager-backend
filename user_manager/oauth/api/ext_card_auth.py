20d40de0abc9deeb3fcddd892ffe2e635301917a
Lukas <lukas@infornautik.de>
2021-12-04 15:32:23 +0100

    feat: Card api for fetching userinfo directly

--- user_manager/oauth/api/ext_card_auth.py
+++ user_manager/oauth/api/ext_card_auth.py
@@ -11,7 +11,8 @@ from starlette.responses import JSONResponse
 from user_manager.common.config import config
 from user_manager.common.models import DbUser
 from user_manager.common.mongo import async_user_collection
-from user_manager.oauth.oauth2 import authorization, ErrorJSONResponse, RedirectResponse, JwtConfigMixin
+from user_manager.oauth.oauth2 import authorization, ErrorJSONResponse, RedirectResponse, JwtConfigMixin, \
+    user_introspection
 from user_manager.oauth.user_helper import UserWithRoles
 from .ext_auth_base import AuthenticateClient
 from .oauth2_helper import oauth2_request
@@ -63,6 +64,29 @@ async def authorize_card(
     if isinstance(resp, RedirectResponse):
         return resp.to_json_response()
     assert not isinstance(resp, JSONResponse)
+
+
+@router.post(
+    '/card/userinfo',
+    tags=['Extension: Card Authentication'],
+    response_model=Dict[str, Any],
+)
+async def check_card(
+        card: CardModel = Body(...),
+        client: dict = Depends(client_auth),
+) -> Dict[str, Any]:
+    """Authorize by card ID."""
+
+    # Find user by card id and authorize by that.
+    user_data = await async_user_collection.find_one({'card_id': card.card_id})
+    if user_data is None:
+        raise HTTPException(404, "Unknown card")
+    user = DbUser.validate_document(user_data)
+    if not user.active:
+        raise HTTPException(400, "User not active")
+
+    user_with_groups = await UserWithRoles.async_load_groups(user, client['_id'])
+    return await user_introspection.async_generate_user_info(user_with_groups, client['allowed_scope'])
 
 
 class UpdateTokenGenerator(JwtConfigMixin):
